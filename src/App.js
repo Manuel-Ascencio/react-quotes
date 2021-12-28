6bdb15151b4a967c2db09d05a1180ad44eb32fe0
Manuelipsu <85pwb927@gmail.com>
2021-12-28 16:36:09 -0600

    uploading the pryect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import User from "./components/User"
+import users from "./users.json"
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const random = (max) =>  Math.floor(Math.random() * max);
+
+  const colors = ["#845EC2", "#FF9671", "#FFC75F", "#2C73D2", "#008F7A","#0089BA", "#008E9B", "#C34A36", "#4B4453", "#AE5D00"];
+
+  const [people, setPeople] = useState(users[random(users.length)]);
+  
+  const background = colors[random(colors.length)];
+  
+  let changeUser = () => {
+    setPeople(users[random(users.length)])
+  }
+
+  return(
+    <main className="body" style={{background}}>
+      <User
+      name={`${people.name.first} ${people.name.last}`} 
+      picture={people.picture.large} 
+      location={`${people.location.city} ${people.location.country}`}
+      phone={people.phone} 
+      email={people.email} 
+      change={changeUser}
+      background={background}
+      />
+      </main>
   );
 }
 
