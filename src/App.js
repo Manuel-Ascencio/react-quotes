3f06a275be3d00b2150b52fe39c2407397ccef90
Manuel-Ascencio <manuelascencioprz@gmail.com>
2022-08-04 13:25:08 -0500

    responsive design

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React from 'react';
-import './App.css';
-import { useState } from "react"
-import quotes from "./quotes.json"
-import QuoteBox from "./components/QuoteBox"
-import Util from "./components/Util"
-
-
-
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import quotes from "./quotes.json";
+import QuoteBox from "./components/QuoteBox";
+import Util from "./components/Util";
 
 function App() {
+  const colors = [
+    "#136066",
+    "#382924",
+    "#db4b23",
+    "#e89d10",
+    "#11644d",
+    "#304878",
+    "#622824",
+    "#412a9c",
+    "#7d2948",
+    "#a84818",
+  ];
 
-  const colors = ["#2C73D2", "#008F7A", "#845EC2", "#4D8076", "#C34A36", "#FF8066", "#4B4453", "#B0A8B9", "#FF6F91", "#926C00"]
+  const [phrase, setPhrase] = useState(quotes[Util(quotes.length)]);
 
-  // const random = (max) => Math.floor(Math.random() * max)
-
-
-  const [ phrase, setPhrase ] = useState(quotes[Util(quotes.length)]);
-
-  const background = colors[Util(colors.length)]
+  const background = colors[Util(colors.length)];
 
   let changeQuote = () => setPhrase(quotes[Util(quotes.length)]);
 
-
-
-  return(
-    <main style={{background}}>
-      <QuoteBox cita={phrase.quote} author={phrase.author} change={changeQuote} color={background} />
+  return (
+    <main style={{ background }}>
+      <QuoteBox
+        cita={phrase.quote}
+        author={phrase.author}
+        image={phrase.image}
+        change={changeQuote}
+        color={background}
+      />
     </main>
   );
 }
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,16 +1,35 @@
-import React from 'react';
-
-function QuoteBox({cita, author, change, color}){
-    return(
-        <section className="quote_card">
-            <i className="fas fa-quote-left" style={{color}}></i>
-            <h3 style={{color}}>{cita}</h3>
-            <p style={{color}}>{author}</p>
-            <div className="row">
-            <button><i className="fas fa-arrow-circle-right" style={{color}} onClick={change}></i></button>
-            </div>
-        </section>
-    );
+import React from "react";
+function QuoteBox({ cita, author, image, change, color }) {
+  return (
+    <div id="quote-box">
+      <i className="fas fa-quote-left icon" style={{ color }}></i>
+      <div className="quote-info">
+        <blockquote>
+          <img src={image} alt="" />
+          <p style={{ color }} id="text">
+            {cita}
+          </p>
+        </blockquote>
+        <h3 style={{ color }} id="author">
+          {author}
+        </h3>
+      </div>
+      <a
+        href="https://twitter.com/intent/tweet"
+        rel="noopener noreferrer"
+        target={"_blank"}
+      >
+        <i
+          className="fa fa-twitter icon-2"
+          style={{ color }}
+          title="Tweet this quote!"
+        />
+      </a>
+      <button onClick={change} style={{ background: color }} id="new-quote">
+        Next
+      </button>
+    </div>
+  );
 }
 
-export default QuoteBox
+export default QuoteBox;
